32ab6ccfca6b6c43e6eab9dd4d05accc09b603ac
Alejo Gomez <alejandro.gomezq97@gmail.com>
2022-03-11 18:03:38 -0500

    Limpiando y organizando el proyecto

---
+++ src/views/MacStudio.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Bootstrap</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'MacStudio',
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
